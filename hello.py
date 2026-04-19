6192bcc5a04eef5989d69c4f5218ad9c075130c9
Tuanha <huyynhanhtuan1401@gmail.com>
2026-04-19 10:38:18 +0700

    Day 1: Hello AI on Linux - First program

---
+++ hello.py
@@ -0,0 +1,6 @@
+import sys
+ngay_hoc = 1
+print("Hello AI! Nice to meet you teacher Gwen")
+print(f"Running in Linux")
+print(f"Python version: { sys.version.split()[0] }")
+print(f"Em hoc AI ngay thu: {ngay_hoc}")
